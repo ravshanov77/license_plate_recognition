760e3ad2f0ca53ba8c632db77ecff790816ce21c
Farrukh <farruxlynx@gmail.com>
2024-01-19 10:16:34 +0500

    main file

--- main.py
+++ main.py
@@ -14,3 +14,28 @@ model = YOLO('yolov8s')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 
+
+#Preparing the video
+video = 'single_car.mp4'
+
+cap = cv2.VideoCapture(video)
+
+if not cap.isOpened():
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+#Detecting the cars
+car_detect = model(cap)
