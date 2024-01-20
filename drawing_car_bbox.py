0bc9a40ebf5e7ac7b4f45f095c354586f6572118
Farrukh <farruxlynx@gmail.com>
2024-01-20 16:15:53 +0500

    additional

--- drawing_car_bbox.py
+++ drawing_car_bbox.py
@@ -18,3 +18,24 @@ def draw_bbox(img, labels):
     x += 1
 
   return img
+
+
+#drawing bbox for licence plate
+def draw_bbox_lp(img, labels):
+
+  x = 0
+  while x < len(labels):
+    for i in labels:
+      img = cv.rectangle(img, (int(i[0]), int(i[1])), (int(i[2]), int(i[3])), color=[0,255,0], thickness=int(img.shape[0] ** .2))
+
+      img = cv.putText(img, 'number', org=(int(i[0]), int(i[1]) - 5),
+                       fontFace=int(img.shape[0] ** .11),
+                       fontScale=int(img.shape[0] ** .1),
+                       color=[255,0,0],
+                       thickness=int(img.shape[0] ** .15))
+
+      font = cv.FONT_HERSHEY_SIMPLEX
+
+    x += 1
+
+  return img
