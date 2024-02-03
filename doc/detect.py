2771f603f71fd44aab87a8fec8640a458a6e6562
Farrukh <farruxlynx@gmail.com>
2024-02-03 20:01:28 +0500

    detect

--- doc/detect.py
+++ doc/detect.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 
 
 #Defining YOLO model
-model = YOLO('yolov8s')
+model = YOLO('models/yolov8s')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
