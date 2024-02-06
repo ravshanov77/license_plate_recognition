339030c4e4e2d14801779a40209a784b8ba13fd0
Farrukh <farruxlynx@gmail.com>
2024-02-06 20:12:28 +0500

    video detect

--- doc/detectVideo.py
+++ doc/detectVideo.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 
 
 #Defining YOLO model
-model = YOLO('models\yolov8s.pt')
+model = YOLO('models/yolov8s.pt')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
